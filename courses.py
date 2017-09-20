69282ce87d5765451df51dd52db7c04aceda89f2
Nitzan Shpigel <nizan1212@gmail.com>
2017-09-20 03:14:51 +0300

    Add course scripts

---
+++ courses.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+""" Getting information about courses """
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# dept = input("Choose Department: \n")
+browser = webdriver.Chrome()
+browser.get('https://bgu4u.bgu.ac.il/pls/scwp/!app.gate?app=ann')
+time.sleep(1)  # Wait for page to be loaded
+
+element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//frame[@name='main']")))
+browser.switch_to.frame(element)
+browser.find_elements_by_xpath("//a[@href='javascript:goType(1)']")[0].click()
+# browser.switch_to_frame(0)
+
+find_course = browser.find_elements_by_xpath("//input[@id='on_course_department']")[0]
+find_course.clear()
+find_course.send_keys("205")
+browser.find_elements_by_xpath("//input[@id='GOPAGE2']")[0].click()
+
+course_list = browser.find_elements_by_xpath("//td[@class='SearchFormTextBold']/a")
+counter = 0
+for i in range(0, len(course_list)):
+    course_list[i].click()
+    source = browser.page_source
+    if u"מדעי המחשב" in source:
+        counter += 1
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Course Number: " + str(counter) + "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        info = browser.find_element_by_xpath("//ul[@class='props']")
+        category = info.find_elements_by_xpath("//p[@class='key']")
+        values = info.find_elements_by_xpath("//p[@class='val']")
+        for j in range(0, len(values)):
+            print "*" + str(category[j].text.encode('utf-8')) + str(values[j].text.encode('utf-8'))
+        print "\n"
+    browser.find_elements_by_xpath("//img[@name='buttonBack']")[0].click()  # go back
+    course_list = browser.find_elements_by_xpath("//td[@class='SearchFormTextBold']/a")  # restore list
--- form.py
+++ form.py
@@ -1,32 +1,69 @@
 # coding=utf-8
-""" Fills google form automatically """
+""" Fills google form automatically with random details.
+    Note that Editing should be done according to the form structs (mostly in order of inputs and submit button) """
 
 from selenium import webdriver
 import time
 import random
+from faker import Faker
 
-names = ["Dan", "Tomer", "Dana", "Ron", "Chen", "Yaniv", "Mor", "Adi", "Ofir", "Sapir", "Eden"]
-fnames = ["Israeli", "Rozenberg", "Biton", "Ashkenazi", "Mizrahi", "Haddad", "Sharon", "Oz", "Cohen", "Levi", "Berger"]
+def name():
+    names = [u"דן", u"תומר", u"דנה", u"רון", u"חן", u"יניב", u"מור", u"עדי", u"אופיר", u"ספיר", u"עדן", u"נעמה",
+             u"מיכל", u"אביב", u"גיל", u"ניצן", u"דניאל", u"שרה", u"אבי", u"איתי", u"רונן", u"אור", u"שני", u"יובל",
+             u"מאיר", u"רותם", u"אלון", u"שגיא", u"נתי"]
+    return names[random.randint(0, len(names) - 1)]
 
-browser = webdriver.Chrome()
-""" Good example of form which use all abilities"""
-browser.get('https://docs.google.com/forms/d/e/1FAIpQLSefsV3eQqDp7Cl0u9sM2j7H2WSFoS09Ns7oCF-h2cGGc2YyiQ/viewform?hl=en&formkey=dDliTk5XU1R4RUMtb2c1WDZxWHNENmc6MQ&fbzx=3205651701266619319#gid=0')
-time.sleep(2)  # Wait for page to be loaded
+def fname():
+    fnames = [u"ישראלי", u"רוזנברג", u"ביטון", u"אשכנזי", u"מזרחי", u"חדד", u"שרון", u"עוז", u"כהן", u"לוי",
+              u"ברגר", u"סגל", u"בראון", u"לייבוביץ'", u"מיכאלשווילי", u"אליאס", u"שמעוני", u"קליינשטיין", u"שטיין",
+              u"רגב", u"רוזן"]
+    return fnames[random.randint(0, len(fnames) - 1)]
 
-browser.find_element_by_class_name("ss-q-short").send_keys(names[random.randint(0,10)])  # one line input
-browser.find_element_by_class_name("ss-q-long").send_keys(fnames[random.randint(0,10)])  # text box input
-browser.find_element_by_class_name("ss-q-checkbox").click()  # multiply choice
+def city():
+    cities = [u"תל אביב", u"ירושלים", u"חיפה", u"באר שבע", u"אילת", u"מודיעין", u"אשדוד", u"אשקלון", u"קריית שמונה",
+              u"נתניה", u"חדרה", u"רמת גן", u"נתניה", u"כפר סבא", u"הוד השרון", u"הרצליה", u"פתח תקווה", u"גבעתיים"]
+    return cities[random.randint(0, len(cities) - 1)]
+
+def age():
+    return random.randint(21, 40)
+
+def phone():
+    return random.choice(["0504", "0520", "0545", "0573"]) + str(''.join(map(str, random.sample(xrange(0, 9), 6))))
+
+def email():
+    fake = Faker()
+    return fake.email()
+
+def money():
+    return random.randrange(2000, 8000, 200)
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+browser = webdriver.Chrome(chrome_options=chrome_options)
+browser.get('https://docs.google.com/forms/d/e/1FAIpQLScZghfyFqyvu80S5U4YzyaXX-cnne-wV45RptRWaAL1i_qa0g/viewform')
+time.sleep(1)  # Wait for whole page to be loaded
+
+''' Text Input '''
+textbox = browser.find_elements_by_xpath("//input[@type='text']")
+textbox[0].send_keys("None")                    # free text
+textbox[1].send_keys(money())                   # Money
+textbox[2].send_keys(name() + " " + fname())    # name + family name
+textbox[3].send_keys(age())                     # age
+textbox[4].send_keys(city())                    # city
+textbox[5].send_keys(phone())                   # phone
+
+''' Choices '''
+choices = browser.find_elements_by_xpath("//input[@type='radio']")
+for c in choices:
+    c.click()  # marks all last options
+
+''' Submit '''
+#submit = browser.find_elements_by_xpath("//div[@role='button']")[1].click()
+#browser.close()
+
+"""
 browser.find_element_by_class_name("ss-q-radio").click()  # single choice
+browser.find_element_by_class_name("ss-q-checkbox").click()  # multiply choice
 value = browser.find_elements_by_xpath("//select/option[2]")[0].click()  # choose from list
-
-boxes = browser.find_elements_by_xpath("//*[@role='radio']")
-for i in range(0, len(boxes)-2):
-    i = i + random.randint(0, 2)  # random choice, one of the 3 last options
-    boxes[i].click()
 """
-submit = browser.find_elements_by_xpath("//*[@type='submit']")
-for s in submit:
-     s.click()
-"""
-
-
